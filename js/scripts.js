739f90e62e6471dec6aa3f84d29f724f21f1526c
catherine bradley and neil peachey <neil@>
2018-08-08 14:13:44 -0700

    it will add a working calculator with more buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,45 @@
-//
-// var number1 = parseInt(prompt("what is the first number?"));
-// var number2 = parseInt(prompt("what is the second number?"));
-//
-//
+var add = function(inputNumber1, inputNumber2) {
+return inputNumber1 + inputNumber2;
+};
+
+var subtract = function(inputNumber1, inputNumber2) {
+return inputNumber1 - inputNumber2;
+};
+
+var multiply = function(inputNumber1, inputNumber2) {
+return inputNumber1 * inputNumber2;
+};
+
+var divide = function(inputNumber1, inputNumber2) {
+return inputNumber1 / inputNumber2;
+};
+
+$(document).ready(function() {
+  $("#cal").submit(function() {
+  var inputNumber1 = parseFloat($("#number1").val());
+  var inputNumber2 = parseFloat($("#number2").val());
+
+$(".addresult").append(add(inputNumber1, inputNumber2));
+$(".subtractresult").append(subtract(inputNumber1, inputNumber2));
+
+event.preventDefault();
+
+  // alert(add(inputAdd1, inputAdd2));
+  // alert(subtract(inputAdd1, inputAdd2));
+  // alert(multiply(inputAdd1, inputAdd2));
+  // alert(divide(inputAdd1, inputAdd2));
+  });
+});
+
+
+
+// $(".result").append.parseInt(inputAdd1 + inputAdd2());
+// event.preventDefault();
+
 // var add = function(number1, number2) {
-//   return number1 + number2;
+// return number1 + number2;
 // };
+
 // var subtract = function(number1, number2) {
 //    return number1 - number2;
 // };
@@ -21,15 +55,15 @@
 // var resultsMultiply = multiply(number1, number2);
 // var resultsDivide = divide(number1, number2);
 
-var height = parseFloat(prompt("what is your height?"));
-var weight = parseFloat(prompt("what is your weight"));
-
-var BMI = function(weight, height) {
-  return weight / (height * height);
-};
-
-var resultsBMI = BMI(weight, height);
-
-// alert(number1 + " and " + number2 + ' together is ' + resultsAdd + " " +
-//  resultsSubtract + " " + resultsMultiply + " " + resultsDivide);
-alert("your BMI is " + resultsBMI);
+// var height = parseFloat(prompt("what is your height?"));
+// var weight = parseFloat(prompt("what is your weight"));
+//
+// var BMI = function(weight, height) {
+//   return weight / (height * height);
+// };
+//
+// var resultsBMI = BMI(weight, height);
+//
+// // alert(number1 + " and " + number2 + ' together is ' + resultsAdd + " " +
+// //  resultsSubtract + " " + resultsMultiply + " " + resultsDivide);
+// alert("your BMI is " + resultsBMI);
